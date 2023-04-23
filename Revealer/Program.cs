c42e73050cdbfb7200ca7c0c68fcaad48fe58628
SergeyFM <github@svistunov.su>
2023-04-23 12:39:09 +0300

    Add project files.

---
+++ Revealer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace Revealer {
+    internal class Program {
+        static void Main(string[] args) {
+
+            new Revealer();
+
+        }
+
+
+
+    }
+}
---
+++ Revealer/Revealer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revealer {
+    /// <summary>
+    /// Reveals who called
+    /// new Revealer();
+    /// </summary>
+    public class Revealer {
+        public Revealer(string message = "",
+                [CallerMemberName] string memberName = "",
+                [CallerFilePath] string sourceFilePath = "",
+                [CallerLineNumber] int sourceLineNumber = 0) {
+            string msg = message != "" ? $" msg: {message}" : "";
+            
+            Console.WriteLine($"{memberName}() from {sourceFilePath} line {sourceLineNumber}" + msg);
+        }
+
+    }
+}
