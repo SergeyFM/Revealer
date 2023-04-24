918c61cff10597e453dbf68d35796942144afd08
SergeyFM <github@svistunov.su>
2023-04-24 20:33:29 +0300

    Updates so that the class would better reveal the coller

--- Revealer/Revealer.cs
+++ Revealer/Revealer.cs
@@ -1,22 +1,41 @@
 ﻿global using Revealer;
+using System;
+using System.Linq;
 using System.Runtime.CompilerServices;
 
 namespace Revealer {
     /// <summary>
     /// Reveals who called
-    /// new Reveal();
+    /// Insert "new Reveal();" anywhere. Used for debugging and research.
     /// </summary>
+
     public class Reveal {
-        public Reveal(string message = "",
+        public Reveal(string message = "", bool VIP = false,
                 [CallerMemberName] string memberName = "",
                 [CallerFilePath] string sourceFilePath = "",
                 [CallerLineNumber] int sourceLineNumber = 0) {
             string msg = message != "" ? $"\t{message}" : "";
             string file = sourceFilePath.Split(@"\").Last();
 
-            //Console.WriteLine($"{memberName}() from {file} line {sourceLineNumber}" + msg);
-            Console.WriteLine($"{file}:{sourceLineNumber} \t {memberName}()" + msg);
+            string file_lineN = $"{file}:{sourceLineNumber} \t {memberName}()";
+            if (_last_file_lineN != file_lineN) {
+                if (_last_file_lineN_Count > 0) {
+                    Console.WriteLine($"* {_last_file_lineN} was called {_last_file_lineN_Count} times more]");
+                }
+                if (VIP) Console.Write(">>>>>>>>>>>> "); // mark very important positions with a easy-to-notice line
+                Console.WriteLine($"{file}:{sourceLineNumber} \t {memberName}()" + msg);
+
+                _last_file_lineN = file_lineN;
+                _last_file_lineN_Count = 0;
+
+            } else {
+                _last_file_lineN_Count++; // don't show anything when the call is the same
+            }
         }
+
+        public static string _last_file_lineN = "";
+        public static int _last_file_lineN_Count = 0;
+
 
     }
 }
--- Test_run/Program.cs
+++ Test_run/Program.cs
@@ -6,7 +6,13 @@ namespace Test_run {
             Console.WriteLine("Hello, World!");
 
             new Reveal();
-            new Reveal("Some random message");
+            new Reveal("Some random message", true);
+
+            for(int i = 0; i < 10; i++) {
+                new Reveal();
+            }
+
+            new Reveal();
         }
     }
 }
